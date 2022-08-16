c6898d59b006c1aceca83340ff3ae4a9f863a30b
Satoshi Matsushima <>
2022-08-16 23:26:05 +0900

    merged files

---
+++ components/layout.js
@@ -0,0 +1,15 @@
+import Footer from '@/components/footer'
+import Header from '@/components/header'
+
+const Layout = ({ children }) => {
+  return (
+    <>
+      <Header />
+
+      <main>{children}</main>
+
+      <Footer />
+    </>
+  )
+}
+export default Layout
