a05b51608dfc4045abc4e84af3a3c95f8ac5a4f2
Satoshi Matsushima <>
2022-08-20 12:36:57 +0900

    created blog page

---
+++ pages/blog/[slug].js
@@ -0,0 +1,121 @@
+import { getPostBySlug, getAllSlugs } from 'lib/api'
+import { extractText } from 'lib/extract-text'
+import Meta from '@/components/meta'
+import Container from '@/components/container'
+import PostHeader from '@/components/post-header'
+import PostBody from '@/components/post-body'
+import {
+  TwoColumn,
+  TwoColumnMain,
+  TwoColumnSidebar,
+} from '@/components/two-column'
+import Image from 'next/image'
+// ローカル代替アイキャッチ画像
+import { eyecatchLocal } from 'lib/constants'
+
+import ConvertBody from '@/components/convert-body'
+import PostCategories from '@/components/post-categories'
+import { getPlaiceholder } from 'plaiceholder'
+import { prevNextPost } from 'lib/prev-next-post'
+import Pagination from '@/components/pagination'
+
+const Post = ({
+  title,
+  publish,
+  content,
+  eyecatch,
+  categories,
+  description,
+  prevPost,
+  nextPost,
+}) => {
+  return (
+    <Container>
+      <Meta
+        pageTitle={title}
+        pageDesc={description}
+        pageImg={eyecatch.url}
+        pageImgW={eyecatch.width}
+        pageImgH={eyecatch.height}
+      />
+      <article>
+        <PostHeader title={title} subtitle="Blog Article" publish={publish} />
+        <figure>
+          <Image
+            // Imageコンポーネントにkey属性を持たせ、Imageコンポーネントは記事ごとに異なるコンポーネントであると明示し、記事内のページネーションから遷移時にblurや画像の切り替わり遅延を防ぐ
+            key={eyecatch.url}
+            src={eyecatch.url}
+            alt=" "
+            layout="responsive"
+            width={eyecatch.width}
+            height={eyecatch.height}
+            sizes="(min-width: 1152px) 1152px,100vw"
+            priority
+            placeholder="blur"
+            blurDataURL={eyecatch.blurDataURL}
+          />
+        </figure>
+
+        <TwoColumn>
+          <TwoColumnMain>
+            <PostBody>
+              <ConvertBody contentHTML={content} />
+            </PostBody>
+          </TwoColumnMain>
+
+          <TwoColumnSidebar>
+            <PostCategories categories={categories} />
+          </TwoColumnSidebar>
+        </TwoColumn>
+        <Pagination
+          prevText={prevPost.title}
+          prevUrl={`/blog/${prevPost.slug}`}
+          nextText={nextPost.title}
+          nextUrl={`/blog/${nextPost.slug}`}
+        />
+      </article>
+    </Container>
+  )
+}
+
+export async function getStaticPaths() {
+  const allSlugs = await getAllSlugs(5)
+
+  return {
+    paths: allSlugs.map(({ slug }) => `/blog/${slug}`),
+    fallback: 'blocking',
+  }
+}
+
+export async function getStaticProps(context) {
+  const slug = context.params.slug
+  const post = await getPostBySlug(slug)
+
+  if (!post) {
+    return { notFound: true }
+  } else {
+    const description = extractText(post.content)
+    const eyecatch = post.eyecatch ?? eyecatchLocal
+
+    const { base64 } = await getPlaiceholder(eyecatch.url)
+    eyecatch.blurDataURL = base64
+
+    const allSlugs = await getAllSlugs()
+    const [prevPost, nextPost] = prevNextPost(allSlugs, slug)
+
+    return {
+      props: {
+        title: post.title,
+        publish: post.publishDate,
+        content: post.content,
+        eyecatch: eyecatch,
+        categories: post.categories,
+        description: description,
+        prevPost: prevPost,
+        nextPost: nextPost,
+      },
+    }
+  }
+}
+
+export default Post
---
+++ pages/blog/index.js
@@ -0,0 +1,38 @@
+import Meta from '@/components/meta'
+import Container from 'components/container'
+import Hero from '@/components/hero'
+import { getAllPosts } from 'lib/api'
+import Posts from '@/components/posts'
+import { getPlaiceholder } from 'plaiceholder'
+// ローカル代替アイキャッチ画像
+import { eyecatchLocal } from 'lib/constants'
+
+export default function Blog({ posts }) {
+  return (
+    <Container>
+      <Meta pageTitle="Blog Archives" pageDesc="Blog Archives" />
+      <Hero title="Blog" subtitle="技術系中心に記事を投稿しています" />
+
+      <Posts posts={posts} />
+    </Container>
+  )
+}
+
+export async function getStaticProps() {
+  const posts = await getAllPosts()
+
+  for (const post of posts) {
+    if (!post.hasOwnProperty('eyecatch')) {
+      post.eyecatch = eyecatchLocal
+    }
+
+    const { base64 } = await getPlaiceholder(post.eyecatch.url)
+    post.eyecatch.blurDataURL = base64
+  }
+
+  return {
+    props: {
+      posts: posts,
+    },
+  }
+}
