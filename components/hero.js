62a66dbb2140c1056d54df54ccef1b697c6957fa
Satoshi Matsushima <work.satoshi1993@gmail.com>
2022-11-23 21:19:41 +0900

    added animations (title, lead)

--- components/hero.js
+++ components/hero.js
@@ -1,13 +1,25 @@
 import Image from 'next/image'
+import { useEffect, useState } from 'react'
 import styles from 'styles/hero.module.css'
 
-export default function Hero({ title, subtitle, textCenter = false }) {
+import AnimatedText from './animatedText'
+
+export default function Hero({
+  title,
+  subtitle,
+  textCenter = false,
+  glitchTitle = false,
+}) {
   return (
     <div className={styles.flexContainer}>
       <div className={styles.text}>
-        <h1 className={styles.title}>{title}</h1>
+        <h1
+          className={glitchTitle ? styles.glitchTitle : styles.normalTitle}
+          data-text={title}>
+          {title}
+        </h1>
         <p className={textCenter ? styles.subtitleCenter : styles.subtitle}>
-          {subtitle}
+          <AnimatedText animatedText={subtitle} />
         </p>
       </div>
     </div>
