3d251c451f2f7e9631e8cc653ed7b0d736844bfe
Satoshi Matsushima <work.satoshi1993@gmail.com>
2022-12-07 20:24:52 +0900

    added gtag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,10 +5,13 @@ import '@fortawesome/fontawesome-svg-core/styles.css'
 import { config } from '@fortawesome/fontawesome-svg-core'
 import { NextPage } from 'next'
 import { AppProps } from 'next/app'
-import { ReactElement, ReactNode } from 'react'
+import { useRouter } from 'next/router'
+import Script from 'next/script'
+import { ReactElement, ReactNode, useEffect } from 'react'
 
 import Layout from '../components/layout'
 import Transition from '../components/transition'
+import * as gtag from '../lib/gtag'
 
 config.autoAddCss = false
 
@@ -21,16 +24,46 @@ type AppPropsWithLayout = AppProps & {
 }
 
 function MyApp({ Component, pageProps }: AppPropsWithLayout) {
+  const router = useRouter()
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
   const getLayout = Component.getLayout || ((page: ReactElement) => page)
 
   return (
-    <Layout>
-      {getLayout(
-        <Transition>
-          <Component {...pageProps} />
-        </Transition>,
-      )}
-    </Layout>
+    <>
+      <Script
+        async
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_MEASUREMENT_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){dataLayer.push(arguments);}
+                  gtag('js', new Date());
+                  gtag('config', '${gtag.GA_MEASUREMENT_ID}', {
+                    page_path: window.location.pathname,
+                  });`,
+        }}
+      />
+      <Layout>
+        {getLayout(
+          <Transition>
+            <Component {...pageProps} />
+          </Transition>,
+        )}
+      </Layout>
+    </>
   )
 }
 
