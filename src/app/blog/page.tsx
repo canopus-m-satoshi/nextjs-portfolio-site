5119f51512838aaccdd27edd1aefc2c29b40dced
Satoshi Matsushima <work.satoshi1993@gmail.com>
2024-04-11 10:48:54 +0900

    blogページのレイアウト修正

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,15 +1,36 @@
 'use client'
 
+import useSWR from 'swr'
+
 import BlogList from '../../components/BlogList'
 import Hero from '../../components/hero'
 
+interface Post {
+  id: number
+  path: string
+  title: string
+  published_at: string
+  body_updated_at?: string
+}
+
+const fetcher = (url: string) => fetch(url).then((res) => res.json())
 export default function Home() {
+  const { data, error } = useSWR<{ articles: Post[] }>('/api/zenn', fetcher)
+
+  if (error) return <div>Failed to load blog posts.</div>
+  if (!data) return null
+
+  const posts = data.articles
+
   return (
     <>
       {/* <Meta pageTitle="Work" pageDesc="制作実績紹介ページ" /> */}
       <Hero title="Blog" subtitle="技術系中心に記事を投稿しています" />
-      <p>記事をクリックするとzennへ遷移します</p>
-      <BlogList />
+      <p>
+        私がzennに投稿した記事を表示しています。
+        各記事をクリックするとzennへ遷移します。
+      </p>
+      <BlogList posts={posts} />
     </>
   )
 }
--- src/components/BlogList.tsx
+++ src/components/BlogList.tsx
@@ -1,44 +1,41 @@
-'use client'
-
 import Link from 'next/link'
-import useSWR from 'swr'
 
 import styles from '../styles/blog-list.module.css'
 import ConvertDate from './ConvertDate'
 
-interface Post {
-  id: number
-  path: string
-  emoji: string
-  title: string
-  published_at: string
+interface Props {
+  posts: {
+    id: number
+    path: string
+    title: string
+    published_at: string
+    body_updated_at?: string
+  }[]
 }
 
-const fetcher = (url: string) => fetch(url).then((res) => res.json())
-
-export default function BlogList() {
-  const { data, error } = useSWR<{ articles: Post[] }>('/api/zenn', fetcher)
-
-  if (error) return <div>Failed to load blog posts.</div>
-  if (!data) return <div>Loading...</div>
-
-  const posts = data.articles
-
+export default function BlogList({ posts }: Props) {
   return (
-    <>
-      <ul className={styles.lists}>
-        {posts.map((post) => (
-          <li key={post.id}>
-            <Link href={`https://zenn.dev/${post.path}`} target="blank">
-              <span>{post.emoji}</span>
-              <p className={styles.title}>{post.title}</p>
+    <ul className={styles.lists}>
+      {posts.map((post) => (
+        <li key={post.id}>
+          <Link
+            href={`https://zenn.dev/${post.path}`}
+            target="blank"
+            className={styles.link}>
+            <p className={styles.title}>{post.title}</p>
+            <p>
+              投稿日：
+              <ConvertDate dateISO={post.published_at} />
+            </p>
+            {post.body_updated_at && (
               <p>
-                <ConvertDate dateISO={post.published_at} />
+                更新日：
+                <ConvertDate dateISO={post.body_updated_at} />
               </p>
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </>
+            )}
+          </Link>
+        </li>
+      ))}
+    </ul>
   )
 }
