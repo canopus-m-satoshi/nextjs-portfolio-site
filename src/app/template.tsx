beb2937564576bcf80c19d8cd11419e41cd2df25
Satoshi Matsushima <work.satoshi1993@gmail.com>
2024-04-11 17:07:34 +0900

    ページ遷移トランジションの変更

---
+++ src/app/template.tsx
@@ -0,0 +1,83 @@
+'use client'
+import { AnimatePresence, motion, useReducedMotion } from 'framer-motion'
+import { usePathname } from 'next/navigation'
+import React from 'react'
+
+import { useTransitionFix } from '../lib/useTransitionFix'
+
+export default function Template({ children }: { children: React.ReactNode }) {
+  const transitionCallback = useTransitionFix()
+  const shouldReduceMotion = useReducedMotion()
+  const pathname = usePathname()
+
+  const getVariants = (page: string) => {
+    switch (page) {
+      case '/':
+        return {
+          out: { opacity: 0, y: 40, x: 40, transition: { duration: 0.75 } },
+          in: {
+            opacity: 1,
+            y: 0,
+            x: 0,
+            transition: { duration: 0.75, delay: 0.5 },
+          },
+        }
+      case '/about':
+        return {
+          out: { opacity: 0, y: -40, x: -40, transition: { duration: 0.75 } },
+          in: {
+            opacity: 1,
+            y: 0,
+            x: 0,
+            transition: { duration: 0.75, delay: 0.5 },
+          },
+        }
+      case '/blog':
+        return {
+          out: { opacity: 0, y: 40, x: -40, transition: { duration: 0.75 } },
+          in: {
+            opacity: 1,
+            y: 0,
+            x: 0,
+            transition: { duration: 0.75, delay: 0.5 },
+          },
+        }
+      case '/work':
+        return {
+          out: { opacity: 0, y: -40, x: 40, transition: { duration: 0.75 } },
+          in: {
+            opacity: 1,
+            y: 0,
+            x: 0,
+            transition: { duration: 0.75, delay: 0.5 },
+          },
+        }
+      case '/contact':
+        return {
+          out: { opacity: 0, scale: 0.8, transition: { duration: 0.75 } },
+          in: {
+            opacity: 1,
+            scale: 1,
+            transition: { duration: 0.75, delay: 0.5 },
+          },
+        }
+      default:
+        return {
+          out: { opacity: 0, transition: { duration: 0.75 } },
+          in: { opacity: 1, transition: { duration: 0.75, delay: 0.5 } },
+        }
+    }
+  }
+
+  const variants = getVariants(pathname)
+
+  return (
+    <motion.div
+      {...(!shouldReduceMotion && { variants })}
+      animate="in"
+      initial="out"
+      exit="out">
+      {children}
+    </motion.div>
+  )
+}
