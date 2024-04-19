5ccce1e78fc2363bdb7d9615f9f561f1aa4b6b15
Satoshi Matsushima <work.satoshi1993@gmail.com>
2024-04-19 15:58:04 +0900

    LinkButtonコンポーネントの修正
    矢印の方向を選択できるように変更

--- src/app/work/[id]/page.tsx
+++ src/app/work/[id]/page.tsx
@@ -1,6 +1,10 @@
+import Link from 'next/link'
+import { FaChevronLeft } from 'react-icons/fa6'
+
 import CategoryList from '../../../components/CategoryList/CategoryList'
 import Description from '../../../components/Description/Description'
 import Hero from '../../../components/Hero/Hero'
+import LinkButton from '../../../components/LinkButton/LinkButton'
 import SiteView from '../../../components/SiteView/SiteView'
 import { getWorkDetailsBySlug } from '../../../lib/microCMS/client'
 import styles from './workDetails.module.css'
@@ -23,6 +27,10 @@ export default async function WorkDetails({
       <div className={styles.details}>
         <SiteView img={img} alt={title} />
         <Description role={role} url={url} description={description} />
+      </div>
+
+      <div className={styles.backButton}>
+        <LinkButton href="/work" text="Back" isLeftArrow />
       </div>
     </>
   )
--- src/components/LinkButton/LinkButton.tsx
+++ src/components/LinkButton/LinkButton.tsx
@@ -1,18 +1,24 @@
 import Link from 'next/link'
 import { FaChevronRight } from 'react-icons/fa'
+import { FaChevronLeft } from 'react-icons/fa6'
 
 import styles from './LinkButton.module.css'
 
 type Props = {
   href: string
   text: string
+  isLeftArrow?: boolean
 }
 
-export default function LinkButton({ href, text }: Props) {
+export default function LinkButton({ href, text, isLeftArrow = false }: Props) {
   return (
     <span className={styles.link}>
       <Link href={href}>
-        <FaChevronRight color={'#fff'} />
+        {isLeftArrow ? (
+          <FaChevronLeft color={'#fff'} />
+        ) : (
+          <FaChevronRight color={'#fff'} />
+        )}
         <span>{text}</span>
       </Link>
     </span>
